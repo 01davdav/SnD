5e570351ba2b1f499d168626b243073d2478f278
Susu <35997313+susu-susu@users.noreply.github.com>
2018-01-31 15:17:04 +0100

    eventCards

---
+++ scripts/events.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class events : MonoBehaviour {
+
+    public int[] costs = new int[4];
+    private string text;
+    public int[] brezels = new int[5];
+    public int id;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
