84fa0396efe88b5dd8fbfbe1ecdaf8ea938c5502
DavidTS_lap <10davdav@gmail.com>
2018-02-01 08:05:42 +0100

    basicshiat

---
+++ scripts/hand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hand : MonoBehaviour
+{
+	public List<GameObject> handCards = new List<GameObject>();
+	public GameObject resourceCard;
+	
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 5; i++)
+		{
+			GameObject card = Instantiate(resourceCard, new Vector3((float)i, 1, (float)-i), Quaternion.identity) as GameObject;
+			card.transform.parent = gameObject.transform;
+			card.GetComponent<resources>().type = i;
+			handCards.Add(card);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- scripts/resources.cs
+++ scripts/resources.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class resources : MonoBehaviour {
 
-    private int type;
+    public int type;
+	public Sprite Drugs;
+	public Sprite Fame;
+	public Sprite GottesSegen;
+	public Sprite Love;
+	public Sprite Money;
+	
+	public Sprite[] sprites = new Sprite[5];
+	
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		sprites[0] = Drugs;
+		sprites[1] = Fame;
+		sprites[2] = GottesSegen;
+		sprites[3] = Love;
+		sprites[4] = Money;
+		updateCard();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
+	}
+
+	public void updateCard()
+	{
+		this.GetComponent<SpriteRenderer>().sprite = sprites[type];
 	}
 }
